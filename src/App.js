f2242dd4ff7beca742553bc751119b940ed73342
Ernest Klingler~ <ernestbklingler@gmail.com~>
2025-10-21 22:58:35 -0400

    TMDB integration 2

--- src/App.js
+++ src/App.js
@@ -1844,7 +1844,7 @@ const selectTMDBMovie = async (movie) => {
               onChange={(e) => setEditingMaster({...editingMaster, title: e.target.value})}
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
               placeholder="Enter movie title"
-          </div>
+         </div>
   <label className="block text-sm font-medium text-gray-700 mb-2">Poster URL (optional)</label>
   <input
     type="text"
